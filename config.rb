7105a768272bc7ad51cadd1cabc05c777e84f1e1
Claudio Vallejo <hola@claudiovallejo.mx>
2016-11-28 11:52:01 -0600

    Cleaned configuration and added Contentful configuration.

--- config.rb
+++ config.rb
@@ -1,22 +1,6 @@
 ###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
-
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
 # General configuration
+###
 
 # Reload the browser automatically whenever files change
 configure :development do
@@ -24,21 +8,21 @@ configure :development do
 end
 
 ###
-# Helpers
+# Page configuration
 ###
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+# Layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
 
-# Build-specific configuration
-configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
+###
+# Contentful configuration
+###
+activate :contentful do |f|
+  f.access_token = '595538fecbbc00253d9baf3defb922f09047601b17bab296fe8ef82d24592adf'
+  f.space = { content: 'rvnl2qm3k12r' }
+  f.content_types = {
+    gallery: 'gallery'
+  }
 end
