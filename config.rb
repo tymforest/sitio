b031bedb8f3c612b1c38e7efb93cd1b4519d6cf3
Claudio Vallejo <hola@claudiovallejo.mx>
2016-12-01 17:05:17 -0600

    Added directory indexing, added more (unsuccessful) content pulls, create cda query (unsuccessful).

--- config.rb
+++ config.rb
@@ -7,6 +7,9 @@ configure :development do
   activate :livereload
 end
 
+# Directory Indexes
+activate :directory_indexes
+
 ###
 # Page configuration
 ###
@@ -22,8 +25,12 @@ page '/*.txt', layout: false
 activate :contentful do |f|
   f.access_token = '595538fecbbc00253d9baf3defb922f09047601b17bab296fe8ef82d24592adf'
   f.space = { site: 'rvnl2qm3k12r' }
+  f.cda_query = { include: 10 }
   f.content_types = {
+    products_a: 'products',
+    products_b: 'productos_simple',
+    error: 'error',
     policy: 'policy',
-    products: 'products'
+    contact: 'contact'
   }
 end
