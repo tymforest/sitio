afe14d92a8422f13d1b9b93ee8f14802f92b66dc
Claudio Vallejo <hola@claudiovallejo.mx>
2016-12-01 12:03:42 -0600

    Added policy and product content_types.

--- config.rb
+++ config.rb
@@ -21,8 +21,9 @@ page '/*.txt', layout: false
 ###
 activate :contentful do |f|
   f.access_token = '595538fecbbc00253d9baf3defb922f09047601b17bab296fe8ef82d24592adf'
-  f.space = { content: 'rvnl2qm3k12r' }
+  f.space = { site: 'rvnl2qm3k12r' }
   f.content_types = {
-    gallery: 'gallery'
+    policy: 'policy',
+    products: 'products'
   }
 end
