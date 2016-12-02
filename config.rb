47aaec17ce5e212889b6c18f91768c70038181bb
Claudio Vallejo <hola@claudiovallejo.mx>
2016-12-02 10:22:00 -0600

    Fetching index, stores and product data.

--- config.rb
+++ config.rb
@@ -26,5 +26,7 @@ page '/index.html', layout: 'index'
 activate :contentful do |f|
   f.access_token = '595538fecbbc00253d9baf3defb922f09047601b17bab296fe8ef82d24592adf'
   f.space = { site: 'rvnl2qm3k12r' }
-  f.content_types = { prueba: 'index_page' }
+  f.cda_query = { limit: 1000 }
+  f.all_entries = true
+  f.content_types = { index: "index_page", stores: "store", products_a: "products", products_b: "producto_simple" }
 end
