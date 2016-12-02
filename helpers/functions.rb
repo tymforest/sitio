c0f80d73fe1a1f98a4fbee5dcbc5567beae83efc
Claudio Vallejo <hola@claudiovallejo.mx>
2016-12-02 13:21:19 -0600

    Created a wrap helper function to package store schedules.

--- helpers/functions.rb
+++ helpers/functions.rb
@@ -3,4 +3,36 @@ module Functions
   def phone number
     phone_number = number.gsub(' ', '').gsub('(', '').gsub(')', '')
   end
+  # Schedule Wrapper
+  def wrap store
+    schedules = []
+    weekdays = []
+    saturdays = []
+    data.site.stores.each do |id, s|
+      if s.name === store
+        sc = s.schedule
+        if !sc.weekday_lunch && !sc.weekday_afternoon
+          block = []
+          block.push(sc.weekday_open)
+          block.push(sc.weekday_close)
+          weekdays.push(block)
+        else
+          block = []
+          block.push(sc.weekday_open)
+          block.push(sc.weekday_lunch)
+          weekdays.push(block)
+          block = []
+          block.push(sc.weekday_afternoon)
+          block.push(sc.weekday_close)
+        end
+        block = []
+        block.push(sc.saturday_open)
+        block.push(sc.saturday_close)
+        saturdays.push(block)
+      end
+      schedules.push(weekdays)
+      schedules.push(saturdays)
+      schedules
+    end
+  end
 end
