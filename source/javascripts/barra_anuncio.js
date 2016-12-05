6be869328f8fa79c0bb51e155386df8c0ccb5d30
Claudio Vallejo <hola@claudiovallejo.mx>
2016-12-05 15:28:22 -0600

    Created banner ad script.

---
+++ source/javascripts/barra_anuncio.js
@@ -0,0 +1,7 @@
+var $barra = document.querySelector('.barra-carrusel');
+var flkty = new Flickity( $barra, {
+  autoPlay: 7000,
+  pauseAutoPlayOnHover: false,
+  pageDots: false,
+  draggable: false
+});
