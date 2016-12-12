5fff02c1a8a63aa05d2571474fd429a711a6ed1f
Claudio Vallejo <hola@claudiovallejo.mx>
2016-12-12 09:28:32 -0600

    Activated webhook.

--- config.rb
+++ config.rb
@@ -28,6 +28,7 @@ activate :contentful do |f|
   f.access_token = '595538fecbbc00253d9baf3defb922f09047601b17bab296fe8ef82d24592adf'
   f.space = { site: 'rvnl2qm3k12r' }
   f.cda_query = { include: 10 }
+  f.rebuild_on_webhook = true
   f.all_entries = true
   f.content_types = {
     index_page: "index_page",
