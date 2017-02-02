f663a9b3d42911cf4a8c835132c01a12290ca2a3
Claudio Vallejo <hola@claudiovallejo.mx>
2017-02-02 15:02:57 -0600

    Created gulpfile.js with build optimization tasks.

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp'),
+    htmlmin = require('gulp-htmlmin'),
+    cssmin = require('gulp-csso'),
+    uglify = require('gulp-uglify'),
+    gzip = require('gulp-gzip');
+    sitemap = require('gulp-sitemap'),
+    runSequence = require('run-sequence');
+
+//  Minify .html
+gulp.task('markup', function(){
+  gulp.src('build/**/*.html')
+  .pipe(htmlmin())
+  .pipe(gulp.dest('build'))
+});
+
+//  Minify site.css + gzip site.css
+gulp.task('styles', function(){
+  gulp.src(['build/stylesheets/*.css'])
+  .pipe(cssmin())
+  .pipe(gzip())
+  .pipe(gulp.dest('build/stylesheets'))
+});
+
+//  Minify all.js
+gulp.task('scripts', function(){
+  gulp.src('build/javascripts/*.js')
+  .pipe(uglify())
+  .pipe(gzip())
+  .pipe(gulp.dest('build/javascripts'))
+});
+
+//  Generate sitemap
+gulp.task('sitemap', function() {
+  gulp.src('build/**/*.html', {
+    read: false
+  })
+  .pipe(sitemap({
+    siteUrl: 'http://www.tymforest.com'
+  }))
+  .pipe(gulp.dest('./build'));
+});
+
+// Run previous `gulp` tasks in sequence
+gulp.task('sequence', function(callback) {
+  runSequence(['markup', 'styles', 'scripts', 'sitemap']);
+});
+
+// Build
+gulp.task('build', ['sequence']);
