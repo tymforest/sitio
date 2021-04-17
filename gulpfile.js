c471cf030e731d14feb6a775e6aa7e5a6a3851fe
Claudio Vallejo <hola@claudiovallejo.mx>
2021-04-17 16:04:06 -0400

    Upgraded gulp 3 -> gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,43 @@
-var gulp = require('gulp'),
-    htmlmin = require('gulp-htmlmin'),
-    cssmin = require('gulp-csso'),
-    uglify = require('gulp-uglify'),
-    gzip = require('gulp-gzip');
-    sitemap = require('gulp-sitemap'),
-    runSequence = require('run-sequence');
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const cssmin = require('gulp-csso');
+const uglify = require('gulp-uglify');
+const gzip = require('gulp-gzip');
+const sitemap = require('gulp-sitemap');
 
-//  Minify .html
-gulp.task('markup', function(){
+function minifyHtml(cb) {
   gulp.src('build/**/*.html')
-  .pipe(htmlmin())
-  .pipe(gulp.dest('build'))
-});
+    .pipe(htmlmin())
+    .pipe(gulp.dest('build'));
+  cb();
+}
 
-//  Minify site.css + gzip site.css
-gulp.task('styles', function(){
+function compressCss(cb) {
   gulp.src(['build/stylesheets/*.css'])
-  .pipe(cssmin())
-  .pipe(gzip())
-  .pipe(gulp.dest('build/stylesheets'))
-});
+    .pipe(cssmin())
+    .pipe(gzip())
+    .pipe(gulp.dest('build/stylesheets'))
+  cb();
+}
 
-//  Minify and gzip all .js files
-gulp.task('scripts', function(){
+function compressJs(cb) {
   gulp.src('build/javascripts/*.js')
-  .pipe(uglify())
-  .pipe(gzip())
-  .pipe(gulp.dest('build/javascripts'))
-});
+    .pipe(uglify())
+    .pipe(gzip())
+    .pipe(gulp.dest('build/javascripts'))
+  cb();
+}
 
-//  Generate sitemap
-gulp.task('sitemap', function() {
-  gulp.src('build/**/*.html', {
-    read: false
-  })
-  .pipe(sitemap({
-    siteUrl: 'http://www.tymforest.com'
-  }))
-  .pipe(gulp.dest('./build'));
-});
+function generateSitemap(cb) {
+  gulp.src('build/**/*.html', {read:false})
+    .pipe(sitemap({siteUrl:'http://www.tymforest.com'}))
+    .pipe(gulp.dest('./build'));
+  cb();
+}
 
-// Run previous `gulp` tasks in sequence
-gulp.task('sequence', function(callback) {
-  runSequence('markup', 'styles', 'scripts', 'sitemap');
-});
-
-// Build
-gulp.task('build', ['sequence']);
+exports.default = gulp.series(
+  minifyHtml,
+  compressCss,
+  compressJs,
+  generateSitemap,
+);
